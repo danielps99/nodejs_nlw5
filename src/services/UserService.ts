235e88e7989f4d0adb5c1052da5eab9879b6300f
Daniel <danielps99@gmail.com>
2021-04-24 22:20:36 -0300

    DIA #3   -> Criar Entidade, Repositorio, Service e Controller de User   -> Pequenas correções em geral

---
+++ src/services/UserService.ts
@@ -0,0 +1,33 @@
+import { getCustomRepository } from "typeorm";
+
+import { UserRepository } from "../repositories/UserRepository";
+
+class UserService {
+  private usersRepository: UserRepository;
+
+  constructor() {
+    this.usersRepository = getCustomRepository(UserRepository);
+  }
+
+  async create(email: string) {
+    const userExists = await this.usersRepository.findOne({ email });
+
+    if (userExists) {
+      return userExists;
+    }
+
+    const user = this.usersRepository.create({ email });
+
+    await this.usersRepository.save(user);
+
+    return user;
+  }
+
+  async findByEmail(email: string) {
+    const user = await this.usersRepository.findOne({ email });
+
+    return user;
+  }
+}
+
+export { UserService };
