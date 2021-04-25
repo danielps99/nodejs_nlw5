cb96d2a19dc0216d1b288096f55cb40058677a90
Daniel <danielps99@gmail.com>
2021-04-25 14:39:24 -0300

    DIA #4   Correção do erro... htlm por html

--- src/server.ts
+++ src/server.ts
@@ -11,9 +11,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.set("views", path.join(__dirname, "..", "public"));
-app.engine("htlm", require("ejs").renderFile);
+app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
-
 app.get("/pages/client", (request, response) => {
   return response.render("html/client.html");
   // return response.send("ISSO FUNCIONA");
