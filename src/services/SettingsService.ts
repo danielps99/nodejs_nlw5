40754e65fa17ead2f10244d7481b8d090e84b078
Daniel <danielps99@gmail.com>
2021-04-25 22:40:08 -0300

    DIA #4   -> Atualiza via api o atributo que indica se vai mostrar ou não o chat no site   -> Alteração no codigo do index.html para mostrar ou não o chat de acordo com o atributo da tabela setting

--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -1,4 +1,5 @@
 import { getCustomRepository } from "typeorm";
+import { Setting } from "../entities/Setting";
 import { SettingReposity } from "../repositories/SettingRepository";
 
 interface ISettingCreate {
@@ -32,6 +33,23 @@ class SettingService {
     if (settingsAlreadyExists) {
       throw new Error("Settings with username already exists.");
     }
+  }
+
+  async findByUsername(username: string) {
+    const setting = await this.settingReposity.findOne({username});
+    return setting;
+  }
+
+  async update(username: string, chat: boolean) {
+    //PODERIA USAR O UPDATE DO PROPRIO REPOSITORY, MAS FEZ ASSIM PARA APRENDER QUERYBUILDER
+    const setting = await this.settingReposity.createQueryBuilder()
+    .update(Setting)
+    .set({chat})
+    .where("username = :username", {
+      username
+    })
+    .execute();
+    return setting;
   }
 }
 
